39b4058414e1d8045949010e92c6f8647f29f7d4
LeonidVilcu <vilcu.leo@mail.ru>
2022-12-19 23:33:23 +0100

    js-biglietto-treno-modificato

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere.
 const distance = prompt("numero di chilometri che vuole percorrere");
-
+console.log("distance" , distance , typeof distance);
 //prezzo per Km
 let price = (distance * 0.21);
+console.log("price", price, typeof price);
 
 // l'età del passeggero
 const age = prompt("l'età del passeggero");
+console.log("age", age, typeof age);
+
 
 // va applicato uno sconto del 20% per i minorenni
-if(age <= 18) {
+if(age < 18) {
     let priceDiscount = (price / 100 * 20);
     price = price - priceDiscount;
-    console.log ("price" , price , typeof price);
+    console.log ("price" , price , typeof price);  
 }
 
 // va applicato uno sconto del 40% per gli over 65.
- else if(age >= 65) {
+else if(age >= 65) {
     let priceDiscount = (price / 100 * 40);
     price = price - priceDiscount;
     console.log ("price" , price, typeof price);
@@ -23,7 +26,8 @@ if(age <= 18) {
 
 //prezzo non scontato
 else  {
-    console.log("price" , price , typeof price);
+    console.log("price" + "€", price , typeof price);
 }
 
-document.getElementById("mio_id").innerHTML = price.toFixed(2);
+//stampa prezzo finale
+document.getElementById("mio_id").innerHTML = ("€" + price.toFixed(2));
